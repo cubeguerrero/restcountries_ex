742896d08b723b0fc125ae3c5a2a0a6171a87028
Cuthbert Allan Y. Guerrero <cuthbert.guerrero@gmail.com>
2018-09-25 22:09:15 +0800

    Generate project

---
+++ lib/restcountries_ex.ex
@@ -0,0 +1,18 @@
+defmodule RestcountriesEx do
+  @moduledoc """
+  Documentation for RestcountriesEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RestcountriesEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/restcountries_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule RestcountriesExTest do
+  use ExUnit.Case
+  doctest RestcountriesEx
+
+  test "greets the world" do
+    assert RestcountriesEx.hello() == :world
+  end
+end

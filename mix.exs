1378312999c500e7caadb0195e5eee3f19f1e716
Cuthbert Allan Y. Guerrero <cuthbert.guerrero@gmail.com>
2018-09-29 16:53:28 +0800

    add ex_doc and earmark packages

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule RestcountriesEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/cubeguerrero/restcountries_ex",
       deps: deps()
     ]
   end
@@ -23,6 +24,8 @@ defmodule RestcountriesEx.MixProject do
     [
       {:httpoison, "~> 1.3"},
       {:poison,    "~> 4.0"},
+      {:ex_doc,    "~> 0.18.1"},
+      {:earmark,   "~> 1.2.4"},
 
       # Test dependencies
       {:exvcr, "~> 0.10", only: :test}

eb3c5bb3efc86f80159245bf407c099016c759e7
Cuthbert Allan Y. Guerrero <cuthbert.guerrero@gmail.com>
2018-09-29 16:37:33 +0800

    Start with documents

--- lib/restcountries_ex.ex
+++ lib/restcountries_ex.ex
@@ -1,10 +1,14 @@
 defmodule RestcountriesEx do
   @moduledoc """
   Documentation for RestcountriesEx.
+
+  Rest Countries is a RESTful API to get information about countries.
+  Link: https://restcountries.eu/
   """
   alias RestcountriesEx.Country
 
   defdelegate all(fields \\ []), to: Country
   defdelegate find_by_name(name, full_text \\ false), to: Country
+  defdelegate find_by_country_code(codes), to: Country
   defdelegate find_by_region(region), to: Country
 end
--- lib/restcountries_ex/country.ex
+++ lib/restcountries_ex/country.ex
@@ -10,19 +10,45 @@ defmodule RestcountriesEx.Country do
   ## Examples
 
       iex> RestcountriesEx.Country.all()
-      [%{}, ...]
+      {:ok, [%{}, ...]}
 
   """
   def all(fields \\ []) do
     get("all", [], [params: [fields: prepare_fields(fields)]])
   end
 
+  @doc """
+  Searches a country by name. The full_text option dicates whether the search
+  will be strict.
+
+  ## Examples
+
+      iex> RestcountriesEx.Country.find_by_name("united states")
+      {:ok, [%{alpha2Code: "UM"}, %{alpha2Code: "US"}]}
+
+      iex> RestcountriesEx.Country.find_by_name("united states", true)
+      {:ok, [${alpha2Code: "US"}]}
+
+  """
   def find_by_name(name, full_text \\ false) do
     find_by("name/#{name}", [fullText: full_text])
   end
 
-  def find_by_country_code(code) do
-    find_by("alpha/#{code}")
+  @doc """
+  Searches a country by country code, ISO 3166-1 2-letter or 3-letter country.
+  This returns a list of countries, and uses the /alpha?codes={code} endpoint.
+
+  ## Examples
+
+      iex> RestcountriesEx.Country.find_by_country_code(["co"])
+      {:ok, [%{alpha2Code: "co"}]}
+
+      iex> RestcountriesEx.Country.find_by_country_code(["co", "us", um")
+      {:ok, [${alpha2Code: "co"}]}
+
+  """
+  def find_by_country_code(codes) do
+    get("alpha", [], [params: [codes: prepare_fields(codes)]])
   end
 
   def find_by_currency_code(currency) do
